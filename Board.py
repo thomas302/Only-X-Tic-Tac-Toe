fa98bd98f77dace8d692ed4e3e85b2f329c44c0c
thomas302 <mr.actually12@gmail.com>
2018-06-25 16:45:29 -0700

    Update Board.py

--- Board.py
+++ Board.py
@@ -1,5 +1,23 @@
-import wx
-import wx.html2 as html
-
-def main:
-  
+class Board(self):
+  boards = [(1,[]), (2,[]), (3,[])]
+  deadBoards = []
+  def placeX(self, b, x, y):
+    coordinate = (x , y)
+    boardN = self.boards[b-1][1]
+    if b in deadBoards:
+      return False
+    elif coordinate in boardN:
+      return False
+    else:
+      boardN.append(coordinate)
+      return True
+class Turn(self):
+  def turn(self):
+    b1 = eval(input("Choose Board"))
+    x1 = eval(input("Choose Column"))
+    y1 = eval(input("Choose Row"))
+    Board.placeX(self, b1, x1, y1)
+    
+def main():
+  if __name__ == '__main__':
+    
