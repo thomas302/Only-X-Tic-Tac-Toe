c8524808ec33229c63bb5278429669eb40acc70b
thomas302 <mr.actually12@gmail.com>
2018-06-25 23:28:29 -0700

    Update Board.py

--- Board.py
+++ Board.py
@@ -5,37 +5,65 @@ class Board:
   def placeX(self, b, x, y):
     coordinate = (x , y)
     boardN = self.boards[b-1][1]
-    if b in deadBoards:
-      return False
+    if b in self.deadBoards:
+        print("Board "+str(b)+" is dead")
+        return False
     elif coordinate in boardN:
+      print("X is already there")
       return False
     else:
       boardN.append(coordinate)
+      print("appended")
       return True
   def isBoardDead(self, b):
     boardN = self.boards[b-1][1]
-    row1 = [(1,1),(2,1),(3,1)]
-    row2 = [(1,2),(2,2),(3,2)]
-    row3 = [(1,3),(2,3),(3,3)]
-    col1 = [(1,1),(1,2),(1,3)]
-    col2 = [(2,1),(2,2),(2,3)]
-    col3 = [(3,1),(3,2),(3,3)]
-    rDiagonal = [(1,3)(2,2)(3,1)]
-    lDiagonal = [(3,3)(2,2)(1,1)]
-    deadList = [ row1, row2, row3, col1, col2, col3, rDiagonal, lDiagonal]
+    row1 = [((1,1),(2,1),(3,1))]
+    row2 = [((1,2),(2,2),(3,2))]
+    row3 = [((1,3),(2,3),(3,3))]
+    col1 = [((1,1),(1,2),(1,3))]
+    col2 = [((2,1),(2,2),(2,3))]
+    col3 = [((3,1),(3,2),(3,3))]
+    rDiagonal = [((1,3),(2,2),(3,1))]
+    lDiagonal = [((3,3),(2,2),(1,1))]
+    deadList = []
+    deadList.extend((row1, row2, row3, col1, col2, col3, rDiagonal, lDiagonal))
     for i in range(0,7):
-      if set(deadList[i]).issubset(boardN):
+      if set(tuple(deadList[0][i])).issubset(tuple(boardN)):
         self.deadBoards.append(b)
+        x = "Board "+str(b)+" is dead"
+        return x
       else:
+        return None
+class Game:
+  def gameTextBased(self):
+    turnNumber = 0
+    b = Board()
+    while len(b.deadBoards) < 3:
+      b1 = input("Choose Board")
+      x1 = input("Choose Column")
+      y1 = input("Choose Row")
+      b.placeX(int(b1), int(x1), int(y1))
+      print(b.isBoardDead(int(b1)))
+      turnNumber += 1
+    return turnNumber
+      
+    
+t = Game()
+if __name__ == '__main__':
+  print("Play? Y or N")
+  play = input()
+  if play == "Y" or play == "y":
+    while True:
+      if t.gameTextBased() % 2 == 0:
+        print("Player 2 Won") 
+      else:
+        print("player 1 Won")
+      print("Play Again? Y or N")
+      play1 = input()
+      if play1 == "Y" or play1 == "y":
         pass
-class Turn:
-  def turnTextBased(self):
-    b1 = eval(input("Choose Board"))
-    x1 = eval(input("Choose Column"))
-    y1 = eval(input("Choose Row"))
-    Board.placeX(self, b1, x1, y1)
-    
-    
-def main():
-  if __name__ == '__main__':
-    
+      else:
+        break
+  else:
+    exit()
+  exit()
