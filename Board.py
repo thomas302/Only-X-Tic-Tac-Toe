29229bc52d68d0af2663dc6ce4267d8f44ec91d2
thomas302 <mr.actually12@gmail.com>
2018-06-26 11:58:53 -0700

    switched some lists to sets

--- Board.py
+++ Board.py
@@ -1,10 +1,7 @@
 class Board:
   #b represents a boardthrought the script
-  # the boards list is used as a reference for which board an x is placed on
   boards = [(1,[]), (2,[]), (3,[])]
-  #Dead boards contains the reference number of all boards which have been eliminated
   deadBoards = []
-  #appends value to the correct board 
   def placeX(self, b, x, y):
     coordinate = (x , y)
     boardN = self.boards[b-1][1]
@@ -18,32 +15,26 @@ class Board:
       boardN.append(coordinate)
       print("appended")
       return True
-  #main issue, always returns None
-  #desinged to search list for dead boards, and add them to the dead board list
   def isBoardDead(self, b):
     boardN = self.boards[b-1][1]
-    #coordinate positions of all three in a row combonitions
-    row1 = [((1,1),(2,1),(3,1))]
-    row2 = [((1,2),(2,2),(3,2))]
-    row3 = [((1,3),(2,3),(3,3))]
-    col1 = [((1,1),(1,2),(1,3))]
-    col2 = [((2,1),(2,2),(2,3))]
-    col3 = [((3,1),(3,2),(3,3))]
-    rDiagonal = [((1,3),(2,2),(3,1))]
-    lDiagonal = [((3,3),(2,2),(1,1))]
+    row1 = {(1,1),(2,1),(3,1)}
+    row2 = {(1,2),(2,2),(3,2)}
+    row3 = {(1,3),(2,3),(3,3)}
+    col1 = {(1,1),(1,2),(1,3)}
+    col2 = {(2,1),(2,2),(2,3)}
+    col3 = {(3,1),(3,2),(3,3)}
+    rDiagonal = {(1,3),(2,2),(3,1)}
+    lDiagonal = {(3,3),(2,2),(1,1)}
     deadList = []
     deadList.extend((row1, row2, row3, col1, col2, col3, rDiagonal, lDiagonal))
-    #this part is suppossed append to the list but always returns None
-    for i in range(0,7):
-      if set(tuple(deadList[0][i])).issubset(tuple(boardN)):
+    for i in range(8):
+      if deadList[i].issubset(set(boardN)):
         self.deadBoards.append(b)
-        x = "Board "+str(b)+" is dead"
-        return x
+        print("Board "+str(b)+" is dead")
+        
       else:
-        return None
-#game class contains the variety of iterations of the game and will eventually contain functions for the gui to call
+        pass
 class Game:
-  #text based turns
   def gameTextBased(self):
     turnNumber = 0
     b = Board()
@@ -52,9 +43,9 @@ class Game:
       x1 = input("Choose Column")
       y1 = input("Choose Row")
       b.placeX(int(b1), int(x1), int(y1))
-      print(b.isBoardDead(int(b1)))
+      print(b.boards)
+      b.isBoardDead(int(b1))
       turnNumber += 1
-    #turn number records whether or not P1 or P2 wins
     return turnNumber
       
     
